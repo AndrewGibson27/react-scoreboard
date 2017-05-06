e1dd1148ce42de70bd7881e791c79eeea00b2072
Andrew Gibson <atgibson27@gmail.com>
2017-05-06 14:52:11 -0500

    add some comments

--- server/log.js
+++ server/log.js
@@ -1,6 +1,11 @@
 import winston from 'winston';
 import path from 'path';
 
+/**
+  In production, you're probably not going to want
+  your error logs in a directory like this.
+*/
+
 const LOGGER_FILE_NAME = 'errors.log';
 const LOGGER_FILE_DIR = path.join(process.cwd(), 'server');
 const LOGGER_FILE_PATH = path.join(LOGGER_FILE_DIR, LOGGER_FILE_NAME);
