015abb68094b19b4506436facb0a0b3fadabb967
Andrew Gibson <atgibson27@gmail.com>
2017-06-04 18:27:58 -0500

    get rid of that awful long commnt

--- common/constants.js
+++ common/constants.js
@@ -1,40 +1,7 @@
-/* scoresList
-
-// state
-// actions
-{
-  type: 'UPDATE_SCORES',
-  data: []
-}
-
-*/
-
-/* scoreDetail
-
-// state
-const scoreDetail = {
-  id: '2',
-  homeTeam: '',
-  awayTeam: '',
-  homeScore: 0,
-  awayScore: 0,
-  homeHighScorer: '',
-  awayHighScorer: '',
-  quarter: 1,
-  final: false,
-  timeLeft: '10:03',
-  lastUpdated: false
-};
-
-// actions
-{
-  type: 'UPDATE_SCORE',
-  data: {}
-}
-
-*/
-
-// Posts
 export const LOAD_SCORES_REQUEST = 'LOAD_SCORES_REQUEST';
 export const LOAD_SCORES_SUCCESS = 'LOAD_SCORES_SUCCESS';
 export const LOAD_SCORES_FAILURE = 'LOAD_SCORES_FAILURE';
+
+export const LOAD_SCORE_REQUEST = 'LOAD_SCORE_REQUEST';
+export const LOAD_SCORE_SUCCESS = 'LOAD_SCORE_SUCCESS';
+export const LOAD_SCORE_FAILURE = 'LOAD_SCORE_FAILURE';
