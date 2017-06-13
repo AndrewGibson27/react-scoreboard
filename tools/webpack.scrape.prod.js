e09f42ac7c078e33358c230a199d9536adb237ec
Andrew Gibson <atgibson27@gmail.com>
2017-06-13 00:02:35 -0500

    clean up the stuff we don't want

--- tools/webpack.scrape.prod.js
+++ tools/webpack.scrape.prod.js
@@ -1,14 +1,15 @@
 const webpack = require('webpack');
-const fs =  require('fs');
+const fs = require('fs');
 const path = require('path');
 
 const CONFIG = require('./webpack.base');
-const { SERVER_ENTRY, SERVER_OUTPUT, PUBLIC_PATH }  = CONFIG;
+
+const { SERVER_ENTRY, SERVER_OUTPUT } = CONFIG;
 
 function getExternals() {
   const nodeModules = fs.readdirSync(path.join(process.cwd(), 'node_modules'));
-  return nodeModules.reduce(function (ext, mod) {
-    ext[mod] = 'commonjs ' + mod;
+  return nodeModules.reduce((ext, mod) => {
+    ext[mod] = `commonjs ${mod}`;
     return ext;
   }, {});
 }
@@ -16,28 +17,28 @@ function getExternals() {
 module.exports = {
   target: 'node',
   devtool: 'inline-source-map',
-  entry: path.join(SERVER_ENTRY, 'api', 'scrape'),
+  entry: path.join(SERVER_ENTRY, 'scrape', 'scrape'),
   output: {
     path: SERVER_OUTPUT,
-    filename: 'scrape.js'
+    filename: 'scrape.js',
   },
   externals: getExternals(),
   node: {
     __filename: true,
-    __dirname: true
+    __dirname: true,
   },
   module: {
     loaders: [
       {
         test: /\.json$/,
-        loader: 'json-loader'
+        loader: 'json-loader',
       },
       {
         test: /\.js$/,
         loader: 'babel-loader',
-        exclude: /(node_modules)/
-      }
-    ]
+        exclude: /(node_modules)/,
+      },
+    ],
   },
   plugins: [
     new webpack.BannerPlugin(
@@ -47,8 +48,8 @@ module.exports = {
     new webpack.IgnorePlugin(/\.(css|less|scss|svg|png|jpe?g|png)$/),
     new webpack.optimize.UglifyJsPlugin({
       compress: {
-        warnings: false
-      }
-    })
-  ]
+        warnings: false,
+      },
+    }),
+  ],
 };
